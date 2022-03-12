92a851cf79d665aef7babb55a540180032c9f242
abdurRakib960 <rakibabdur960git@gmail.com>
2022-03-13 01:34:44 +0600

    responsive navBar completed

---
+++ components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import BookmarkIcon from "../public/logo-bookmark.svg";
+import MenuIcon from "../public/preview.gif"
+import Image from "next/image"
+const NavBar = () => {
+    return (
+        <header>
+            <nav className="container flex items-center py-4 mt-4 sm:mt-12">
+                <div className="py-1"><Image src={BookmarkIcon} alt="logo" />
+                </div>
+                <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-bookmark-blue uppercase text-sm">
+                    <li className="cursor-pointer">features</li>
+                    <li className="cursor-pointer">pricing</li>
+                    <li className="cursor-pointer">contact</li>
+                    <button type="button" className="bg-bookmark-red text-white rounded-md px-7 py-3">Login</button>
+                </ul>
+                <div className="flex sm:hidden flex-1 justify-end" >
+                    <Image src={MenuIcon} alt="hambarg icon" width="80" height="80" />
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBar;
